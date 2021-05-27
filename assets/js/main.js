66a1b29cf576a506ecbaeb077910b7d3675e7187
Johansen512 <tekstogtext@gmail.com>
2021-05-27 22:44:10 +0200

    med basis stillads

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var config = {
+
+    type:Phaser.Auto,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+    }
+
+};
+
+var game = new Phaser.Game (config)
+
+function preload () {
+
+
+}
+
+function create () {
+    
+}
