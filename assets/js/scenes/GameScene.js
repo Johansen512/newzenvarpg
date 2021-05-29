66650545a16a4014926415318db34c9e08e30e2a
Johansen512 <tekstogtext@gmail.com>
2021-05-29 20:23:28 +0200

    delt ud som komponenter

---
+++ assets/js/scenes/GameScene.js
@@ -0,0 +1,15 @@
+class GameScene extends Phaser.Scene {
+
+    constructor (){
+        super ('Game')
+
+    }
+
+    preload() {
+
+    }
+
+    create(){
+        
+    }
+}
---
+++ assets/js/scenes/Titlescene.js
@@ -0,0 +1,15 @@
+class TitleScene extends Phaser.Scene {
+
+    constructor (){
+        super ('Title')
+
+    }
+
+    preload() {
+
+    }
+
+    create(){
+        
+    }
+}
