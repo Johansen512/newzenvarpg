ecd85ec2e81999cdba68e3671d83c03f8bf88c8a
Johansen512 <tekstogtext@gmail.com>
2021-06-08 12:32:36 +0200

    fejlfinding ... mangel på samme variablen spawner er udkommenteret for nu

--- assets/js/game_manager/GameManager.js
+++ assets/js/game_manager/GameManager.js
@@ -22,50 +22,37 @@ class GameManager {
     }
 
 
-    parseMapData(){
-        console.log (this.mapData);
+    parseMapData() {
         this.mapData.forEach((layer) => {
-
-            if (layer.name === 'player_locations') {
-
-                layer.objects.forEach ((obj) => {
-                    this.playerLocations.push([obj.x, obj.y]);
-                })
-
-            } else if (layer.name === 'chest_locations') {
-
-                layer.objects.forEach ((obj) => {
-
-                    if (this.chestLocations[obj.properties.spawner]) {
-
-                        this.chestLocations[obj.properties.spawner].push ([obj.x, obj.y]);
-
-                    } else {
-                        this.chestLocations[obj.properties.spawner] = [[obj.x, obj.y]];
-                    }
-                  
-                })
-
-            } else if (layer.name === 'monster_locations') {
-
-                layer.objects.forEach ((obj) => {
-                    if (this.monsterLocations[obj.properties.spawner]) {
-
-                        this.monsterLocations[obj.properties.spawner].push ([obj.x, obj.y]);
-
-                    } else {
-                        this.monsterLocations[obj.properties.spawner] = [[obj.x, obj.y]];
-                    }
-                })
-
-            }
+          if (layer.name === 'player_locations') {
+            layer.objects.forEach((obj) => {
+              this.playerLocations.push([obj.x, obj.y]);
+            });
+          } else if (layer.name === 'chest_locations') {
+            layer.objects.forEach((obj) => {
+                //var spawner = getTiledProperty(obj, 'spawner');
+              if (this.chestLocations[obj.properties.spawner]) {
+                this.chestLocations[obj.properties.spawner].push([obj.x, obj.y]);
+              } else {
+                this.chestLocations[obj.properties.spawner] = [[obj.x, obj.y]];
+              }
+            });
+          } else if (layer.name === 'monster_locations') {
+            layer.objects.forEach((obj) => {
+                //var spawner = getTiledProperty(obj, 'spawner');
+                if (this.monsterLocations[obj.properties.spawner]) {
+                  this.monsterLocations[obj.properties.spawner].push([obj.x, obj.y]);
+                } else {
+                  this.monsterLocations[obj.properties.spawner] = [[obj.x, obj.y]];
+                }
+            });
+          }
         });
 
         console.log (this.playerLocations);
+        console.log (this.monsterLocations);
         console.log (this.chestLocations);
-        console.log (this.monsterLocations); 
-
-    };
+      }
 
     setupEventListener(){
 
@@ -73,7 +60,27 @@ class GameManager {
     }
 
     setupSpawners() {
+        //Create chest spawners
+        Object.keys(this.chestLocations).forEach((key) => {
+            const config = {
+                spawnInterval: 3000,
+                limit: 3,
+                spawnerType: 'CHEST',
+                id: `chest-${key}`,
 
+            };
+
+            const spawner = new Spawner(
+
+                config, 
+                this.chestLocations[key], 
+                this.addChest.bind(this), 
+                this.deleteChest.bind(this)
+                
+                ); 
+                this.spawners [spawner.id] = spawner;
+
+        });
 
     }
 
@@ -82,4 +89,12 @@ class GameManager {
         this.scene.events.emit ('spawnPlayer', location);
 
     }
+
+    addChest(id, chest){
+
+        this.chests[id] = chest;
+        console.log(chest)
+    }
+
+    deleteChest(){}
 }
